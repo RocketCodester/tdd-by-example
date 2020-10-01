3cbe40f70e4f7a74b18cd759d212dd44eb7e9a75
rocketcodester <micstandeven@gmail.com>
2020-10-01 14:15:03 -0500

    Fix typos.

--- src/main/java/guru/springframework/Sum.java
+++ src/main/java/guru/springframework/Sum.java
@@ -4,17 +4,17 @@ package guru.springframework;
  * Created by jt on 10/10/18.
  */
 public class Sum implements Expression {
-    Expression augmend;
-    Expression addmend;
+    Expression augend;
+    Expression addend;
 
-    public Sum(Expression augmend, Expression addmend) {
-        this.augmend = augmend;
-        this.addmend = addmend;
+    public Sum(Expression augend, Expression addend) {
+        this.augend = augend;
+        this.addend = addend;
     }
 
     @Override
     public Money reduce(Bank bank, String to) {
-        int amount = augmend.reduce(bank, to).amount + addmend.reduce(bank, to).amount;
+        int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
     }
 
